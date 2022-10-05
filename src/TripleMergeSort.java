69e4cf4741c1bdb6e19b8413d62d5c47910629c5
Gregory <parfeniuk33@gmail.com>
2022-10-05 19:26:15 +0300

    useless read-only collections was removed

--- src/TripleMergeSort.java
+++ src/TripleMergeSort.java
@@ -37,25 +37,26 @@ public class TripleMergeSort {
     }
 
     private static void tripleMerge(final int[] arr, final int leftIndex, final int midIndex_1, final int midIndex_2, final int rightIndex){
-        final List<Integer> firstSubarray = Collections.unmodifiableList(Arrays.stream(Arrays.copyOfRange(arr, leftIndex, midIndex_1 + 1)).boxed().toList());
-        final List<Integer> secondSubarray = Collections.unmodifiableList(Arrays.stream(Arrays.copyOfRange(arr, midIndex_1+1, midIndex_2 + 1)).boxed().toList());
-        final List<Integer> thirdSubarray = Collections.unmodifiableList(Arrays.stream(Arrays.copyOfRange(arr, midIndex_2+1, rightIndex + 1)).boxed().toList());
-        final List<List<Integer>> subarrays = Collections.unmodifiableList(List.of(firstSubarray, secondSubarray, thirdSubarray));
+        final int[] firstSubarray = Arrays.copyOfRange(arr, leftIndex, midIndex_1 + 1);
+        final int[] secondSubarray = Arrays.copyOfRange(arr, midIndex_1 + 1, midIndex_2 + 1);
+        final int[] thirdSubarray = Arrays.copyOfRange(arr, midIndex_2 + 1, rightIndex + 1);
 
-        final int[] indexes = new int[subarrays.size()+1];
+        final int[][] subarrays = {firstSubarray, secondSubarray, thirdSubarray};
+
+        final int[] indexes = new int[subarrays.length + 1];
         indexes[indexes.length-1] = leftIndex;
-        List<Integer> remainingIndexes = IntStream.range(0, 3).filter(index -> indexes[index] < subarrays.get(index).size()).boxed().toList();
+        List<Integer> remainingIndexes = IntStream.range(0, 3).filter(index -> indexes[index] < subarrays[index].length).boxed().toList();
         while(!remainingIndexes.isEmpty()){
-            final int min = Collections.min(remainingIndexes.stream().map(index -> subarrays.get(index).get(indexes[index])).toList());
+            final int min = Collections.min(remainingIndexes.stream().map(index -> subarrays[index][indexes[index]]).toList());
             for (final int index : remainingIndexes){
-                if (subarrays.get(index).get(indexes[index]) == min){
+                if (subarrays[index][indexes[index]] == min){
                     arr[indexes[indexes.length-1]] = min;
                     ++indexes[indexes.length-1];
                     ++indexes[index];
                     break;
                 }
             }
-            remainingIndexes = IntStream.range(0, 3).filter(index -> indexes[index] < subarrays.get(index).size()).boxed().toList();
+            remainingIndexes = IntStream.range(0, 3).filter(index -> indexes[index] < subarrays[index].length).boxed().toList();
         }
     }
 
