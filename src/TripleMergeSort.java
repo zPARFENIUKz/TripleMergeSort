4dfef26f7560fc4fec674546f94ef95aae81a7d0
Gregory <parfeniuk33@gmail.com>
2022-10-05 16:41:04 +0300

    some simplifications

--- src/TripleMergeSort.java
+++ src/TripleMergeSort.java
@@ -24,8 +24,8 @@ public class TripleMergeSort {
 
     public static void tripleMergeSort(final int[] arr, final int leftIndex, final int rightIndex) throws NullPointerException, ArrayIndexOutOfBoundsException{
         if (arr == null) throw new NullPointerException("arr cannot be null");
-        if (leftIndex < 0 || rightIndex >= arr.length) throw new ArrayIndexOutOfBoundsException("Incorrect indexes was passed");
-        if (leftIndex < rightIndex) {
+        if (leftIndex < 0 || rightIndex >= arr.length) throw new ArrayIndexOutOfBoundsException("Incorrect indexes passed");
+        if (leftIndex < rightIndex){
             final int midIndex_1 = leftIndex + (rightIndex - leftIndex) / 3;
             final int midIndex_2 = leftIndex + ((rightIndex - leftIndex) / 3) * 2;
 
@@ -38,29 +38,25 @@ public class TripleMergeSort {
 
     private static void tripleMerge(final int[] arr, final int leftIndex, final int midIndex_1, final int midIndex_2, final int rightIndex){
         final List<Integer> firstSubarray = Collections.unmodifiableList(Arrays.stream(Arrays.copyOfRange(arr, leftIndex, midIndex_1 + 1)).boxed().toList());
-        final List<Integer> secondSubarray = Collections.unmodifiableList(Arrays.stream(Arrays.copyOfRange(arr, midIndex_1+1, midIndex_2+1)).boxed().toList());
-        final List<Integer> thirdSubarray = Collections.unmodifiableList(Arrays.stream(Arrays.copyOfRange(arr, midIndex_2 + 1, rightIndex+1)).boxed().toList());
+        final List<Integer> secondSubarray = Collections.unmodifiableList(Arrays.stream(Arrays.copyOfRange(arr, midIndex_1+1, midIndex_2 + 1)).boxed().toList());
+        final List<Integer> thirdSubarray = Collections.unmodifiableList(Arrays.stream(Arrays.copyOfRange(arr, midIndex_2+1, rightIndex + 1)).boxed().toList());
         final List<List<Integer>> subarrays = Collections.unmodifiableList(List.of(firstSubarray, secondSubarray, thirdSubarray));
 
         final int[] indexes = new int[subarrays.size()+1];
         indexes[indexes.length-1] = leftIndex;
-        List<Boolean> subarraysElementsRemainingCheck = IntStream.range(0, 3).mapToObj(i -> indexes[i] < subarrays.get(i).size()).toList();
-        while(subarraysElementsRemainingCheck.contains(true)){
-            final List<Integer> unfinishedSubarraysIndexes = IntStream.range(0, 3).filter(subarraysElementsRemainingCheck::get).boxed().toList();
-            final List<Integer> contendersToWrite = Collections.unmodifiableList(unfinishedSubarraysIndexes.stream().map(index -> subarrays.get(index).get(indexes[index])).toList());
-            final int min = Collections.min(contendersToWrite);
-
-            for (int index : unfinishedSubarraysIndexes){
+        List<Integer> remainingIndexes = IntStream.range(0, 3).filter(index -> indexes[index] < subarrays.get(index).size()).boxed().toList();
+        while(!remainingIndexes.isEmpty()){
+            final int min = Collections.min(remainingIndexes.stream().map(index -> subarrays.get(index).get(indexes[index])).toList());
+            for (final int index : remainingIndexes){
                 if (subarrays.get(index).get(indexes[index]) == min){
-                    arr[indexes[3]] = min;
+                    arr[indexes[indexes.length-1]] = min;
+                    ++indexes[indexes.length-1];
                     ++indexes[index];
-                    ++indexes[3];
                     break;
                 }
             }
-            subarraysElementsRemainingCheck = IntStream.range(0, 3).mapToObj(i -> indexes[i] < subarrays.get(i).size()).toList();
+            remainingIndexes = IntStream.range(0, 3).filter(index -> indexes[index] < subarrays.get(index).size()).boxed().toList();
         }
-
     }
 
 }
